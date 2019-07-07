793c42396a3ce26426d8e2cf43e3e360ebe70246
lhaydu <lhaydu@troweprice.com>
2019-07-07 14:43:09 -0400

    Add option to list all snapshots, not just most recent

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -25,7 +25,10 @@ def snapshots():
 @snapshots.command('list')
 @click.option('--project', default=None,
     help="Only volumes for instances with project (tag Project:<name>)")
-def list_snapshots(project):
+@click.option('--all', 'list_all', default=False, is_flag=True,
+    help="List all snapshots fro each volume, not just the most recent")
+def list_snapshots(project, list_all):
+    """List all recent snapshots up to the latest successful snapshot for the volume"""
     instances = filter_instances(project)
 
     for i in instances:
@@ -39,6 +42,8 @@ def list_snapshots(project):
                     s.progress,
                     s.start_time.strftime("%c")
                 )))
+
+                if s.state == "completed" and not list_all: break
     return
 
 @snapshots.command('create')
